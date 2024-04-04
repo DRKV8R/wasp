b2dcd9193f468a0d332a99d568d16ed6bc3e586f
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-04-04 17:07:52 +0200

    Cleanup

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/auth/useAuth.d.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/auth/useAuth.d.ts
@@ -1,3 +1,3 @@
-import type { AuthUser } from 'wasp/auth';
+import type { AuthUser } from '../server/auth/user.js';
 export declare const getMe: () => Promise<AuthUser | null>;
 export default function useAuth(queryFnArgs?: unknown, config?: any): import("@tanstack/react-query").UseQueryResult<AuthUser, Error>;
