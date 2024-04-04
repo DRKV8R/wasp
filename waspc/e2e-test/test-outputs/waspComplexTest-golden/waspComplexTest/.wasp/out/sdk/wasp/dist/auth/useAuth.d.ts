e97d0cab56ff18bfa8f350181335d6c197cbd279
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-04-04 16:55:46 +0200

    Update tests
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/auth/useAuth.d.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/auth/useAuth.d.ts
@@ -1,31 +1,3 @@
 import type { AuthUser } from 'wasp/auth';
 export declare const getMe: () => Promise<AuthUser | null>;
-export default function useAuth(queryFnArgs?: unknown, config?: any): import("@tanstack/react-query").UseQueryResult<{
-    identities: {
-        google: {
-            id: string;
-            data: import("./utils").OAuthProviderData;
-        };
-    };
-    getFirstProviderUserId: () => string;
-    _rawUser: import("@prisma/client/runtime").GetResult<{
-        id: number;
-        username: string;
-        password: string;
-    }, unknown> & {} & {
-        auth: import("@prisma/client/runtime").GetResult<{
-            id: string;
-            userId: number;
-        }, unknown> & {} & {
-            identities: (import("@prisma/client/runtime").GetResult<{
-                providerName: string;
-                providerUserId: string;
-                providerData: string;
-                authId: string;
-            }, unknown> & {})[];
-        };
-    };
-    id: number;
-    username: string;
-    password: string;
-}, Error>;
+export default function useAuth(queryFnArgs?: unknown, config?: any): import("@tanstack/react-query").UseQueryResult<AuthUser, Error>;
